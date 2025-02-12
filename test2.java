81e61a8eb4a5642f945457f61ce672ca339c6a32
summit28 <sumitraskar15@gmail.com>
2025-02-12 14:20:15 +0530

    patterns

--- test2.java
+++ test2.java
@@ -1,4 +1,13 @@
 import java.util.*;
 class test2 {
-	
+    static int num = 4;
+    public static void main(String[] args) {
+        for(int i=1; i<=num;i++){
+            for(int j=1; j<=i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }    
+    }
+    
 }
